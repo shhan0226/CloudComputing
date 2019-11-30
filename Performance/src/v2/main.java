706bef6519d14ba193bcbb57ae2269a8c99984a6
shhan0226 <shhan0226@gmail.com>
2019-12-01 01:16:39 +0900

    Add Java File [Performance]

---
+++ Performance/src/v2/main.java
@@ -0,0 +1,28 @@
+package v2;
+
+public class main {
+	
+	private static int sum() {
+		int sum = 0;
+		for (int j = 0; j < 10 * 1000; j++) {
+			sum += j;
+		}
+		return sum;
+	}
+
+	
+	public static void main(String[] args) {	
+		
+		long t1 = System.nanoTime();
+
+		int result = 0;
+		for (int i = 0; i < 1000 * 1000; i++) { // sole loop
+			result += sum();
+		}
+
+		long t2 = System.nanoTime();
+		System.out.println("Execution time: " + ((t2 - t1) * 1e-9) + " seconds to compute result = " + result);
+	}
+
+
+}
